4c0222127f1fb259e01656e2cdf16c2d76bb1a0c
yustinus margiyuna <iyuszellda@gmail.com>
2025-07-15 14:28:57 +0700

    cors issue

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,6 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
     'allowed_headers' => ['Content-Type', 'X-XSRF-TOKEN', 'Authorization'],
-    'allow_credentials' => true,
     'max_age' => 0,
     'supports_credentials' => true,
 ];
