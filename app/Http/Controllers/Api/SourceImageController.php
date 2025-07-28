410acec94c6d526f60661aa55a007112d3f5f68d
yustinus margiyuna <iyuszellda@gmail.com>
2025-07-28 16:05:27 +0700

    images source

--- app/Http/Controllers/Api/SourceImageController.php
+++ app/Http/Controllers/Api/SourceImageController.php
@@ -10,12 +10,16 @@ class SourceImageController extends Controller
 {
     public function index(): JsonResponse
     {
-        $images = Images::orderBy('product_id')->orderBy('sequence')->get([
-            'product_id',
-            'sequence',
-            'title',
-            'url',
-        ]);
+        $images = Images::orderBy('product_id')
+            ->orderBy('sequence')
+            ->get(['sequence', 'title', 'url'])
+            ->map(function ($image, $i) {
+                return [
+                    'id' => $i +1,
+                    'title'      => "{$image->title} {$image->sequence}",
+                    'url'        => $image->url,
+                ];
+            });
 
         return response()->json($images, 200);
     }
